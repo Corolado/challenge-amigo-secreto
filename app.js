cdb98f23c29c30fcd908eafe6b33eee2ef317605
Corolado <romanyaki99@gmail.com>
2025-02-25 16:31:00 -0300

    Agrega función agregarAmigo()

--- app.js
+++ app.js
@@ -2,3 +2,16 @@
 
 // Array para almacenar nombres de amigos
 let amigos = [];
+
+//Funcion para ingresar nombre en el campo de texto y añardirlo al array
+function agregarAmigo() {
+    let nombre = document.getElementById('nombre').value;
+    // validar que el nombre no este vacio
+    if (nombre.trim() === '') {
+        alert('Por favor, inserte un nombre');
+    } else {
+        amigos.push(nombre);
+    }
+    // limpiar el campo de texto
+    document.getElementById('nombre').value = '';
+}
